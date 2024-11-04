ee9d0852dec3ce6cc3f63159b2540fdb09f1b788
viet <levietviet44266@gmail.com>
2024-11-04 12:04:30 +0700

    first commit

---
+++ next-admin/src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "antd";
+
+export default function Home() {
+  return (
+    <div>
+      <Button title="hello" type="primary">
+        Hello
+      </Button>
+    </div>
+  );
+}
